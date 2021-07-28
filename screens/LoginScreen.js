6981d474a9e3f50b8a61c9f4bfdce0feffc27f28
winclain <winclain91@gmail.com>
2021-07-28 12:08:54 +0300

    new commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,14 @@
 import React,{useState} from 'react';
-import { Text, View, FormControl } from 'native-base';
+import { Text, View, FormControl, ScrollView } from 'native-base';
 
 
 export const LoginScreen = props => {
 
 
     return (
-        <View>
+        <ScrollView>
             
-        </View>
+        </ScrollView>
     );
 }
 
