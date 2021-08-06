c849aed7d60d38f02277be5f59b9029da75636c7
winclain <winclain91@gmail.com>
2021-08-06 18:40:40 +0300

    auth first commit

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,3 +2,17 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export const MainNamvigation = () => {
+
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Home' component={HomeScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,9 +1,7 @@
 export const SIGN_UP = 'SIGN_UP';
 export const SIGN_IN = 'SIGN_IN';
 export const LOGOUT = 'LOGOUT';
-import { Base64 } from "js-base64";
-import axios from "axios";
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const signIn = (email,password) => {
     const res = fetch('http://192.168.88.246:8000/api/login',
